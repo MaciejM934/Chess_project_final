30c94b3e2e69a78de75bb48bec7278efffabcf49
maks <maks@Debian.myguest.virtualbox.org>
2023-06-26 21:09:46 +0200

    gowno

--- Gemfile
+++ Gemfile
@@ -44,7 +44,7 @@ gem "bootsnap", require: false
 
 # Use Sass to process CSS
 # gem "sassc-rails"
-
+gem "sassc"
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
@@ -70,3 +70,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "bootstrap", "~> 5.2"
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,14 +3,19 @@ class SessionController < ApplicationController
   end
 
   def create
-    player = Player.find_by(nickname: params[:session][:index])
-    if player and player.authenticate(params[:session][:password])
-      log_in player
-      redirect_to player
+    if params[:session].present? && params[:session][:index].present? && params[:session][:password].present?
+      player = Player.find_by(nickname: params[:session][:index])
+      if player && player.authenticate(params[:session][:password])
+        log_in player
+        redirect_to player
+      else
+        render 'new'
+      end
     else
       render 'new'
     end
   end
+  
 
   def destroy
     log_out 
